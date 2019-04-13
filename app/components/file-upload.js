1924cb272b041b089b8fb7e68453a9a3afe0094b
Totalfreak <dgkartin@live.dk>
2019-04-13 14:44:43 +0200

    Upload thing

---
+++ app/components/file-upload.js
@@ -0,0 +1,14 @@
+import FileField from 'ember-uploader/components/file-field';
+import Uploader from 'ember-uploader/uploaders/uploader';
+
+export default FileField.extend({
+    filesDidChange(files) {
+        const uploader = Uploader.create({
+            url: this.get(url)
+        });
+
+        if (!Ember.isEmpty(files)) {
+            uploader.upload(files[0]);
+        }
+    }
+});
---
+++ dist/assets/tests.js
@@ -0,0 +1,188 @@
+'use strict';
+
+define("web-miniproject/tests/integration/components/file-upload-test", ["qunit", "ember-qunit", "@ember/test-helpers"], function (_qunit, _emberQunit, _testHelpers) {
+  "use strict";
+
+  (0, _qunit.module)('Integration | Component | file-upload', function (hooks) {
+    (0, _emberQunit.setupRenderingTest)(hooks);
+    (0, _qunit.test)('it renders', async function (assert) {
+      // Set any properties with this.set('myProperty', 'value');
+      // Handle any actions with this.set('myAction', function(val) { ... });
+      await (0, _testHelpers.render)(Ember.HTMLBars.template({
+        "id": "f2JCPKkh",
+        "block": "{\"symbols\":[],\"statements\":[[1,[21,\"file-upload\"],false]],\"hasEval\":false}",
+        "meta": {}
+      }));
+      assert.equal(this.element.textContent.trim(), ''); // Template block usage:
+
+      await (0, _testHelpers.render)(Ember.HTMLBars.template({
+        "id": "YyP7xmNI",
+        "block": "{\"symbols\":[],\"statements\":[[0,\"\\n\"],[4,\"file-upload\",null,null,{\"statements\":[[0,\"        template block text\\n\"]],\"parameters\":[]},null],[0,\"    \"]],\"hasEval\":false}",
+        "meta": {}
+      }));
+      assert.equal(this.element.textContent.trim(), 'template block text');
+    });
+  });
+});
+define("web-miniproject/tests/lint/app.lint-test", [], function () {
+  "use strict";
+
+  QUnit.module('ESLint | app');
+  QUnit.test('adapters/application.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'adapters/application.js should pass ESLint\n\n');
+  });
+  QUnit.test('app.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'app.js should pass ESLint\n\n');
+  });
+  QUnit.test('components/file-upload.js', function (assert) {
+    assert.expect(1);
+    assert.ok(false, 'components/file-upload.js should pass ESLint\n\n7:27 - \'url\' is not defined. (no-undef)\n10:14 - \'Ember\' is not defined. (no-undef)');
+  });
+  QUnit.test('models/post.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'models/post.js should pass ESLint\n\n');
+  });
+  QUnit.test('resolver.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'resolver.js should pass ESLint\n\n');
+  });
+  QUnit.test('router.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'router.js should pass ESLint\n\n');
+  });
+  QUnit.test('routes/application.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'routes/application.js should pass ESLint\n\n');
+  });
+  QUnit.test('serializers/application.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'serializers/application.js should pass ESLint\n\n');
+  });
+});
+define("web-miniproject/tests/lint/templates.template.lint-test", [], function () {
+  "use strict";
+
+  QUnit.module('TemplateLint');
+  QUnit.test('web-miniproject/templates/application.hbs', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'web-miniproject/templates/application.hbs should pass TemplateLint.\n\n');
+  });
+  QUnit.test('web-miniproject/templates/components/file-upload.hbs', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'web-miniproject/templates/components/file-upload.hbs should pass TemplateLint.\n\n');
+  });
+});
+define("web-miniproject/tests/lint/tests.lint-test", [], function () {
+  "use strict";
+
+  QUnit.module('ESLint | tests');
+  QUnit.test('integration/components/file-upload-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'integration/components/file-upload-test.js should pass ESLint\n\n');
+  });
+  QUnit.test('test-helper.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'test-helper.js should pass ESLint\n\n');
+  });
+  QUnit.test('unit/adapters/application-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/adapters/application-test.js should pass ESLint\n\n');
+  });
+  QUnit.test('unit/models/post-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/models/post-test.js should pass ESLint\n\n');
+  });
+  QUnit.test('unit/routes/application-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/routes/application-test.js should pass ESLint\n\n');
+  });
+  QUnit.test('unit/serializers/application-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/serializers/application-test.js should pass ESLint\n\n');
+  });
+});
+define("web-miniproject/tests/test-helper", ["web-miniproject/app", "web-miniproject/config/environment", "@ember/test-helpers", "ember-qunit"], function (_app, _environment, _testHelpers, _emberQunit) {
+  "use strict";
+
+  (0, _testHelpers.setApplication)(_app.default.create(_environment.default.APP));
+  (0, _emberQunit.start)();
+});
+define("web-miniproject/tests/unit/adapters/application-test", ["qunit", "ember-qunit"], function (_qunit, _emberQunit) {
+  "use strict";
+
+  (0, _qunit.module)('Unit | Adapter | application', function (hooks) {
+    (0, _emberQunit.setupTest)(hooks); // Replace this with your real tests.
+
+    (0, _qunit.test)('it exists', function (assert) {
+      let adapter = this.owner.lookup('adapter:application');
+      assert.ok(adapter);
+    });
+  });
+});
+define("web-miniproject/tests/unit/models/post-test", ["qunit", "ember-qunit"], function (_qunit, _emberQunit) {
+  "use strict";
+
+  (0, _qunit.module)('Unit | Model | post', function (hooks) {
+    (0, _emberQunit.setupTest)(hooks); // Replace this with your real tests.
+
+    (0, _qunit.test)('it exists', function (assert) {
+      let store = this.owner.lookup('service:store');
+      let model = store.createRecord('post', {});
+      assert.ok(model);
+    });
+  });
+});
+define("web-miniproject/tests/unit/routes/application-test", ["qunit", "ember-qunit"], function (_qunit, _emberQunit) {
+  "use strict";
+
+  (0, _qunit.module)('Unit | Route | application', function (hooks) {
+    (0, _emberQunit.setupTest)(hooks);
+    (0, _qunit.test)('it exists', function (assert) {
+      let route = this.owner.lookup('route:application');
+      assert.ok(route);
+    });
+  });
+});
+define("web-miniproject/tests/unit/serializers/application-test", ["qunit", "ember-qunit"], function (_qunit, _emberQunit) {
+  "use strict";
+
+  (0, _qunit.module)('Unit | Serializer | application', function (hooks) {
+    (0, _emberQunit.setupTest)(hooks); // Replace this with your real tests.
+
+    (0, _qunit.test)('it exists', function (assert) {
+      let store = this.owner.lookup('service:store');
+      let serializer = store.serializerFor('application');
+      assert.ok(serializer);
+    });
+    (0, _qunit.test)('it serializes records', function (assert) {
+      let store = this.owner.lookup('service:store');
+      let record = store.createRecord('application', {});
+      let serializedRecord = record.serialize();
+      assert.ok(serializedRecord);
+    });
+  });
+});
+define('web-miniproject/config/environment', [], function() {
+  var prefix = 'web-miniproject';
+try {
+  var metaName = prefix + '/config/environment';
+  var rawConfig = document.querySelector('meta[name="' + metaName + '"]').getAttribute('content');
+  var config = JSON.parse(decodeURIComponent(rawConfig));
+
+  var exports = { 'default': config };
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+  return exports;
+}
+catch(err) {
+  throw new Error('Could not read config from meta tag with name "' + metaName + '".');
+}
+
+});
+
+require('web-miniproject/tests/test-helper');
+EmberENV.TESTS_FILE_LOADED = true;
+//# sourceMappingURL=tests.map
