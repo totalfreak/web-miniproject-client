28935b2b0148e8d1ba2d8e7ccb11675a0d38c097
Totalfreak <dgkartin@live.dk>
2019-04-13 13:48:29 +0200

    Heroku

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
   namespace: 'api',
-  host: 'http://localhost:4500'
+  host: 'https://web-miniproject-client.herokuapp.com/'
 });
